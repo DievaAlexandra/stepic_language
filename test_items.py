c996f528ab4b23276a7838bfc4a8bd758642d4fe
aleksandra dieva <dieva.sro@mail.ru>
2023-11-15 23:00:50 +0300

    написан conftest файл и тест для проверки нахождения кнопки добавления товара в корзину

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+import time
 import selenium.webdriver.common.by
 import pytest
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
@@ -5,4 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207
 
 def test_page_item_button_is_visible(browser):
     browser.get(link)
+    time.sleep(30)
+    #проверка страница товара на сайте содержит кнопку добавления в корзину
     assert len(browser.find_elements(selenium.webdriver.common.by.By.CSS_SELECTOR, '.btn-add-to-basket')) > 0
